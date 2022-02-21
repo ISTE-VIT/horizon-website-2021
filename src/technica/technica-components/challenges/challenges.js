332ed3bad0039d7207976f6cee7d14a527fe3f60
sahildotexe <sahilkaling@gmail.com>
2022-02-21 11:37:49 +0530

    Dates -> 2022

--- src/technica/technica-components/challenges/challenges.js
+++
@@ -1,7 +0,0 @@
-import './challenges.scss'
-import './challenge-upper-blue-triangle.svg'
-import './challenge-lower-blue-triangle.svg'
-import './challenge-upper-triangle.svg'
-import './challenge-lower-triangle.svg'
-import './challenge-content-blue-triangle.svg'
-import './echoAR.svg'
